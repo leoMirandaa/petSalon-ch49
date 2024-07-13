f15992fd868db6ffaddbe4e6c8e52e424dd59929
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-07-12 18:22:10 -0700

    feat: add services.js

--- scripts/display.js
+++ scripts/display.js
@@ -1,37 +1,40 @@
-function displayPetCards(){
-    let row="";//creating a var
-    for(let i=0;i<pets.length;i++){//travel the array
-        let pet = pets[i];//getting each pet on the array (one by one)
-        row+=`
-        <div class="pet">
-            <h6>${pet.name}</h6>
-            <p>${pet.age}</p>
-            <p>${pet.gender}</p>
-            <p>${pet.breed}</p>
-        </div>
-        `;
+function displayPetCards() {
+    let row = ""; //creating a var
+    for (let i = 0; i < pets.length; i++) {
+      //travel the array
+      let pet = pets[i]; //getting each pet on the array (one by one)
+      row += `
+          <div class="pet">
+              <h6>${pet.name}</h6>
+              <p>${pet.age}</p>
+              <p>${pet.gender}</p>
+              <p>${pet.breed}</p>
+          </div>
+          `;
     }
-    document.getElementById("pets").innerHTML=row;//display on the HTML
-}
-
-function displayTable(){
-    let row="";//creating a var
-    for(let i=0;i<pets.length;i++){//travel the array
-        let pet = pets[i];//getting each pet on the array (one by one)
-        row+=`
-        <tr id="${i}">
-            <td>${i}</td>
-            <td>${pet.name}</td>
-            <td>${pet.age}</td>
-            <td>${pet.gender}</td>
-            <td>${pet.breed}</td>
-            <td><button onclick="deletePet(${i});">Delete</button></td>
-        </tr>
-        `;
+    document.getElementById("pets").innerHTML = row; //display on the HTML
+  }
+  
+  function displayTable() {
+    let row = ""; //creating a var
+    for (let i = 0; i < pets.length; i++) {
+      //travel the array
+      let pet = pets[i]; //getting each pet on the array (one by one)
+      row += `
+          <tr id="${i}">
+              <td>${i}</td>
+              <td>${pet.name}</td>
+              <td>${pet.age}</td>
+              <td>${pet.gender}</td>
+              <td>${pet.breed}</td>
+              <td><button class='btn btn-light border' onclick="deletePet(${i});">🗑️ Delete</button></td>
+          </tr>
+          `;
     }
-    document.getElementById("petTable").innerHTML=row;
-}
-
-function displayPetCount() {
-    document.getElementById("total").innerHTML="Total:" + pets.length;
-}
+    document.getElementById("petTable").innerHTML = row;
+  }
+  
+  function displayPetCount() {
+    document.getElementById("total").innerHTML = "Total:" + pets.length;
+  }
+  
--- scripts/services.js
+++ scripts/services.js
@@ -1,16 +1,71 @@
+// Create another page named services.html and create another form to register services (description, price).
+// Create the constructor for the services objects.
+//  Create validations and notifications for the new form using jQuery.
+//  Clear the form after the registration of the services using jQuery.
+
+// hidding validation messages
+$(".descriptionValidationMesssage").hide()
+$(".priceValidationMessage").hide()
+
+// creating the constructor
+
 function Service(description, price) {
   this.description = description;
   this.price = price;
 }
 
-function register() {
-  let inputDescription = $("#txtDescription").val();
-  console.log(inputDescription);
 
-  // get inputPrice
+
+// Do validations
+function isValidService(service) {
+  let isValidDescription = true;
+  let isValidPrice = true;
+  
+  // description validation
+  if(service.description == "") { //is empty?
+    isValidDescription = false;
+    $("#txtDescription").addClass("alert-error")
+     $(".descriptionValidationMesssage").css("color","red").show()
+  }else {
+     $(".descriptionValidationMesssage").hide()
+     $("#txtDescription").removeClass("alert-error")
+    isValidDescription = true
+  }
+  
+  // price validation
+  if(service.price == "") {
+    isValidPrice = false;
+    $("#txtPrice").addClass("alert-error")
+    $(".priceValidationMessage").css("color","red").show()
+  }else {
+    $(".priceValidationMessage").hide()
+    $("#txtPrice").removeClass("alert-error")
+
+    isValidPrice = true
+  }
+
+  return isValidDescription && isValidPrice
+}
+
+function register() {
+  // getting description and price from inputs
+  let inputDescription = $("#txtDescription").val();
+  let inputPrice = $("#txtPrice").val()
+  
+  console.log("inputDescription: ", inputDescription);
+  console.log("inputPrice: ", inputPrice);
+
 
   // create a new Obj
   let newService = new Service(inputDescription, inputPrice);
+  console.log("isValid", isValidService(newService))
+  
+  if(isValidService(newService)){
+    console.log("isValid", isValidService(newService))
+    // saveService(newService)
+    $("#txtDescription").val("");
+    $("#txtPrice").val("")
+    // $("input:text, input[type='number']").val("");
 
-  // Do validations
+  }
 }
